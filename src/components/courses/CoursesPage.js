329571f0c02d26bb8f654051a8559c28d1f90301
James Lovejoy <james.lovejoy2@gmail.com>
2020-04-25 17:58:50 -0500

    added 404page and courses page to site

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+//could make a function and use useState and useEffect but instead
+//going with class component format for this page
+class CoursesPage extends React.Component {
+  render() {
+    return <h2>Courses</h2>;
+  }
+}
+
+export default CoursesPage;
