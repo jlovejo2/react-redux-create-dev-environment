3375a254b9ca9101abef98ac8243a558af42d6e0
James Lovejoy <james.lovejoy2@gmail.com>
2020-04-25 17:49:37 -0500

    created navbar and routes for the pages in that navbar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./components/App";
+import "./index.css";
 
-function Hi() {
-  //handy way to get a spot to show up in the browser.  Will happen as long as dev tool is open in browser
-  /*debugger;*/
-  return <p>Hi.</p>;
-}
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("app")
+);
 
-render(<Hi />, document.getElementById("app"));
+//handy way to get a spot to show up in the browser.  Will happen as long as dev tool is open in browser
+/*debugger;*/
