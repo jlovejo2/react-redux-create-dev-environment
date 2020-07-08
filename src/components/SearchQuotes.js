891fd69cdd127bf3988c92588476346012f6e6cd
James Lovejoy <james.lovejoy2@gmail.com>
2020-07-07 20:36:41 -0500

    fixed searchQuotes.js error

--- src/components/SearchQuotes.js
+++ src/components/SearchQuotes.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { loadRonSwansonQuotes } from "../redux/actions/ronSwansonApiActions";
 import { connect } from "react-redux";
